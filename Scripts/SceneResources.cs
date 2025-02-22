0182f719d71c0d0f5aa6a73296abcf0f9c5ec78e
Tebaah <e2nd@protonmail.com>
2025-02-22 01:15:12 -0300

    feat: adds class resources for switching between screens

---
+++ Scripts/SceneResources.cs
@@ -0,0 +1,21 @@
+using Godot;
+using System;
+
+public partial class SceneResources : Node
+{
+    #region Variables
+
+    const string homeScreen = "res://Screen/home_screen.tscn";
+    const string selectionScreen = "res://Screen/selection_screen.tscn";
+    const string gameScreen = "res://Screen/game_screen.tscn";
+
+    #endregion
+
+    #region Godot Methods
+
+    #endregion
+
+    #region Custom Methods
+
+    #endregion
+}
