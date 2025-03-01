96d0feeaca14d9ba7cb726ab9528ede4babbf195
Tebaah <e2nd@protonmail.com>
2025-03-01 19:34:09 -0300

    feat: add input method

--- Scripts/Player/PlayerStatusWalking.cs
+++ Scripts/Player/PlayerStatusWalking.cs
@@ -9,6 +9,23 @@ public partial class PlayerStatusWalking : PlayerStatusBase
 
     #region Godot Methods   
 
+    public override void OnPhysicsProcess(double delta)
+    {
+        player.velocity.X = (float)(Input.GetAxis("Left", "Right") * player.speed * delta);
+        player.Velocity = player.velocity;
+        player.MoveAndSlide();
+
+        player.animations.Play("Walk");
+    }
+
+    public override void OnInput(InputEvent @event)
+    {
+        if (!Input.IsActionPressed("Left") && !Input.IsActionPressed("Right"))
+        {
+            stateMachine.ChangeTo("StateIdle");
+        }
+    }
+
     #endregion  
 
     #region Methods 
