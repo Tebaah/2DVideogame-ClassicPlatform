f2ff86598cad0da5a246307a0994e6604afca14a
Tebaah <e2nd@protonmail.com>
2025-03-01 17:50:55 -0300

    feat: add base class of player state

---
+++ Scripts/Player/PlayerStatusBase.cs
@@ -0,0 +1,37 @@
+using Godot;
+using System;
+
+public partial class PlayerStatusBase : StateBase
+{
+    #region Variables
+
+    public PlayerController player;
+    public double gravity = ProjectSettings.GetSetting("physics/2d/default_gravity").AsSingle();
+
+    #endregion
+
+    #region Godot Methods
+
+    public override void AtStartState()
+    {
+        if (nodeToControl == null)
+        {
+            GD.PrintErr("nodeToControl is null in AtStartState");
+            return;
+        }
+        
+        player = nodeToControl.GetNode<PlayerController>(nodeToControl.GetPath());
+
+        if (player == null)
+        {
+            GD.PrintErr("player is null in AtStartState");
+            return;
+        }
+    }
+
+    #endregion
+
+    #region Methods
+
+    #endregion
+}
