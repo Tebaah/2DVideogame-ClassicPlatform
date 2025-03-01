aa383d51e4378c136a8a8ea845c8ca100a40432d
Tebaah <e2nd@protonmail.com>
2025-03-01 19:02:31 -0300

    feat: add class player status walking

---
+++ Scripts/Player/PlayerStatusWalking.cs
@@ -0,0 +1,17 @@
+using Godot;
+using System;
+
+public partial class PlayerStatusWalking : PlayerStatusBase
+{
+    #region Variables
+
+    #endregion
+
+    #region Godot Methods   
+
+    #endregion  
+
+    #region Methods 
+
+    #endregion
+}
