1a98f6566122d0afaf1f74ac5980ddbc2d1c8962
Tebaah <e2nd@protonmail.com>
2025-03-01 18:48:50 -0300

    feat: add animation varibale for player

--- Scripts/Player/PlayerController.cs
+++ Scripts/Player/PlayerController.cs
@@ -3,4 +3,22 @@ using System;
 
 public partial class PlayerController : Node
 {
+    #region Variables
+
+    public AnimationPlayer animations;
+
+    #endregion
+
+    #region Godot Methods
+
+    public override void _Ready()
+    {
+        animations = GetNode<AnimationPlayer>("AnimationPlayer");
+    }
+
+    #endregion
+
+    #region Methods
+    
+    #endregion
 }
