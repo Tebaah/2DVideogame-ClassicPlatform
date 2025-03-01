90b313f3b96014d51fd3cbcc039bf01a58e913c7
Tebaah <e2nd@protonmail.com>
2025-03-01 18:45:37 -0300

    feat: add class player status idle

---
+++ Scripts/Player/PlayerStatusIdle.cs
@@ -0,0 +1,20 @@
+using Godot;
+using System;
+
+public partial class PlayerStatusIdle : PlayerStatusBase
+{
+    #region Variables
+
+    #endregion
+
+    #region Godot Methods   
+
+    public override void AtStartState()
+    {
+    }
+    #endregion  
+
+    #region Methods 
+
+    #endregion
+}
