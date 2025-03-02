b79593362d0daea9785fbc000201528b5b4c2bfa
Tebaah <e2nd@protonmail.com>
2025-03-01 23:16:17 -0300

    feat: add status falling base

---
+++ Scripts/Player/PlayerStatusFalling.cs
@@ -0,0 +1,17 @@
+using Godot;
+using System;
+
+public partial class PlayerStatusFalling : PlayerStatusGravity
+{
+    #region Variables
+
+    #endregion
+
+    #region Godot Methods
+
+    #endregion
+
+    #region Custom Methods
+
+    #endregion
+}
