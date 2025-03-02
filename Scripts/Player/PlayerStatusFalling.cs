1eb973b437d06c23e6fba8300058148bc43a08c7
Tebaah <e2nd@protonmail.com>
2025-03-01 23:22:09 -0300

    feat: add class player status falling

--- Scripts/Player/PlayerStatusFalling.cs
+++ Scripts/Player/PlayerStatusFalling.cs
@@ -9,6 +9,25 @@ public partial class PlayerStatusFalling : PlayerStatusGravity
 
     #region Godot Methods
 
+    public override void OnPhysicsProcess(double delta)
+    {
+        if (player.velocity.Y > 0 && player.IsOnFloor())
+        {
+            if (Input.IsActionJustPressed("Left") || Input.IsActionJustPressed("Right"))
+            {
+                stateMachine.ChangeTo("StateWalk");
+            }
+            else
+            {
+                stateMachine.ChangeTo("StateIdle");
+            }
+        }
+
+        ApplyGravity(delta);
+        player.Velocity = player.velocity;
+        player.MoveAndSlide();
+    }
+
     #endregion
 
     #region Custom Methods
