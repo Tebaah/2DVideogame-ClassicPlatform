98dc563fea32419d7d3b75b7983cf454728ad292
Tebaah <e2nd@protonmail.com>
2025-03-24 22:21:46 -0300

    feat: add gravity

--- Scripts/Characters/Enemy/EnemyController.cs
+++ Scripts/Characters/Enemy/EnemyController.cs
@@ -3,4 +3,24 @@ using System;
 
 public partial class EnemyController : CharacterBody2D
 {
+    #region Variables
+    [Export] public double speed;
+    [Export] public double jumpForce;
+    public Vector2 velocity;
+    public AnimationPlayer animations;
+
+    #endregion
+
+    #region Godot Methods
+
+    public override void _Ready()
+    {
+        animations = GetNode<AnimationPlayer>("AnimationPlayer");
+    }
+
+    #endregion
+
+    #region Methods
+    
+    #endregion
 }
--- Scripts/Characters/Enemy/EnemyStateIdle.cs
+++ Scripts/Characters/Enemy/EnemyStateIdle.cs
@@ -3,4 +3,25 @@ using System;
 
 public partial class EnemyStateIdle : EnemyStateGravity
 {
+    #region Variables
+
+    #endregion
+
+    #region Godot Methods   
+
+    public override void OnPhysicsProcess(double delta)
+    {
+        enemy.velocity.X = 0;
+        ApplyGravity(delta);
+        enemy.Velocity = enemy.velocity;
+        enemy.MoveAndSlide();
+
+        // enemy.animations.Play("Idle"); TODO: create idle animation
+    }
+
+    #endregion  
+
+    #region Methods 
+
+    #endregion
 }
