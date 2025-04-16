3c827ff4ab54cc89c4dde663e26164356266ced1
Tebaah <e2nd@protonmail.com>
2025-04-15 22:04:15 -0400

    feat: add camera configuration

--- Scripts/Characters/Player/PlayerController.cs
+++ Scripts/Characters/Player/PlayerController.cs
@@ -11,6 +11,9 @@ public partial class PlayerController : CharacterBody2D
     public AnimationPlayer animations;
     public AudioStreamPlayer2D audio;
     private Sprite2D _sprite;
+    private Camera2D _camera;
+
+    private LevelManager _levelManager;
 
     #endregion
 
@@ -18,9 +21,17 @@ public partial class PlayerController : CharacterBody2D
 
     public override void _Ready()
     {
+        // Initialize variables
         animations = GetNode<AnimationPlayer>("AnimationPlayer");
         audio = GetNode<AudioStreamPlayer2D>("AudioStreamPlayer2D");
         _sprite = GetNode<Sprite2D>("Sprite2D");
+        _camera = GetNode<Camera2D>("Camera2D");
+
+        // Initialize the level manager
+        _levelManager = GetParent().GetNode<LevelManager>("../Level1");
+
+        _camera.LimitLeft = _levelManager.LimitLeft;
+        _camera.LimitRight = _levelManager.LimitRight;
     }
 
     public override void _PhysicsProcess(double delta)
