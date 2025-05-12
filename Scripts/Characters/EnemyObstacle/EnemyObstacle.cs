133f11577e10a0448af019cd045a464a776b79a8
Tebaah <e2nd@protonmail.com>
2025-05-11 21:33:50 -0400

    feat(RockEnemy): add basic structure

---
+++ Scripts/Characters/EnemyObstacle/EnemyObstacle.cs
@@ -0,0 +1,14 @@
+using Godot;
+using System;
+
+public partial class EnemyObstacle : Area2D
+{
+    #region Variables
+    #endregion
+
+    #region Godot Methods
+    #endregion
+
+    #region Custom Methods
+    #endregion
+}
