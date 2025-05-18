27d32db87184c4402cfccdab430703ed4b9f2c9a
Tebaah <e2nd@protonmail.com>
2025-05-17 22:19:30 -0400

    feat(DisplayManager.cs): add canvas layer properties

---
+++ Scripts/Screens/HomeScreenState.cs
@@ -0,0 +1,14 @@
+using Godot;
+using System;
+
+public partial class HomeScreenState : StateBase
+{
+    #region Variables
+    #endregion
+
+    #region Godot Methods
+    #endregion
+
+    #region Custom Methods
+    #endregion
+}
